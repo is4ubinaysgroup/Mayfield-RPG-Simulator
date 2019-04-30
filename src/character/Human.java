56b581684da6beddbb6eaa9a53e6bd51218aaed0
Fujirose <50026704+Fujirose@users.noreply.github.com>
2019-04-30 14:10:52 -0400

    Update Human.java

--- src/character/Human.java
+++ src/character/Human.java
@@ -1,4 +1,22 @@
 package character;
+/*
++ MELEETYPE: int
++ RANGEDTYPE: int
++ ALLTYPE: int
+- damage: int[]
+- range: int[]
+- critical chance: double
+- weaponType: int
+
+
+---
+
++getDamage(): int
++setDamage(int)
++getCritialChance(): double
++setCritialChance(double)
+
+*/
 public class Human
 {
 
