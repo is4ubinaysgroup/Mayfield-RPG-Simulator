d7770e6558c5652afce0cb113792c5f4f3d3add2
Fujirose <50026704+Fujirose@users.noreply.github.com>
2019-04-30 13:42:02 -0400

    Item.Java
    Version one. Seems completed.

--- src/items/Item.java
+++ src/items/Item.java
@@ -1,4 +1,34 @@
+/**
+  4/30/2017
+  Version: Fujirose-May30th-Edition
+ */
 package items;
-public class Item
-{
+
+/**
+ * @author Fujirose
+ *item Class is abstract
+ *only contains the String name.
+ */
+public abstract class Item {
+
+	/**
+	 * 
+	 */
+	private String name;
+	
+	public Item() {
+		// TODO Auto-generated constructor stub
+	}
+
+	//Getters and Setters
+	public String getName() 
+	{
+		return name;
+	}
+
+	public void setName(String name) 
+	{
+		this.name = name;
+	}
+
 }
