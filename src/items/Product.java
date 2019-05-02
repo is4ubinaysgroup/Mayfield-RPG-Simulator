9edb2ce3ec464509a260ff3062aab024d67e30d3
minafang71 <50028052+minafang71@users.noreply.github.com>
2019-05-02 14:08:11 -0400

    Add files via upload

---
+++ src/items/Product.java
@@ -0,0 +1,37 @@
+// Mina Fang
+// completed 05-02
+
+public class Product extends Item
+{
+
+	static final int ARMOR = 1;
+	static final int CAPACITY = 2;
+	static final int CRITICALCHANCE = 3;
+	static final int HEALTH = 4;
+	static final int MAXHEALTH = 5;
+	
+	private int upgradeType;
+	private int increase;
+	
+	public Product () {} // default constructor
+	
+	public Product (String name, int upgradeType, int increase)
+	{
+		
+		this.setName(name);
+		this.upgradeType = upgradeType;
+		this.increase = increase;
+		
+	} // Product constructor
+	
+	
+	// ------- set and get methods -------
+	
+	public void setUpgradeType (int upgradeType) { this.upgradeType = upgradeType; } // setUpgradeType method
+	public int getUpgradeType () { return upgradeType; } //
+
+	
+	public void setIncrease (int increase) { this.increase = increase; } // setIncrease method
+	public int getIncrease () { return increase; } // getIncrease method
+	
+} // Upgrade class
