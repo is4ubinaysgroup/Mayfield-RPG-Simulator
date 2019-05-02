efd8a5c4a66e380a2286d0517f4b810746df633b
Fujirose <50026704+Fujirose@users.noreply.github.com>
2019-05-02 14:04:29 -0400

    Update 1
    Finished for matthew

--- src/character/Human.java
+++ src/character/Human.java
@@ -1,23 +1,59 @@
 package character;
 /*
-+ MELEETYPE: int
-+ RANGEDTYPE: int
-+ ALLTYPE: int
-- damage: int[]
-- range: int[]
-- critical chance: double
-- weaponType: int
-
-
----
-
-+getDamage(): int
-+setDamage(int)
-+getCritialChance(): double
-+setCritialChance(double)
-
+@Author: Carson Fujita
+@Contributor:
+human class sets the requirements and data for the rest of the classes
 */
+
 public class Human
 {
+  //ints
+  private int health;
+  private int defense;
+  private int maxHealth;
+  private int movement;
+  private int equippedWeapon;
+  
 
+  //Gets and Sets
+  public int getHealth()
+  {
+    return this.health;
+  }
+  
+  public void setHealth(int health)
+  {
+    this.health = health;
+  }
+  
+  public int getDefense()
+  {
+    return this.defense;
+  }
+  
+  public void setDefense(int defense)
+  {
+    this.defense = defense;
+  }
+  
+  public int getMaxHealth()
+  {
+    return this.maxHealth;
+  }
+  
+  public void setMaxHealth(int maxHealth)
+  {
+    this.maxHealth = maxHealth;
+  }
+  
+  public int getMovement()
+  {
+    return this.movement;
+  }
+  
+  public void setMovement(int movement)
+  {
+    this.movement = movement;
+  }
 }
+
