dc9dd668d353b00a273de593692f165fd15a7704
Fujirose <50026704+Fujirose@users.noreply.github.com>
2019-05-02 14:19:44 -0400

    asdf

--- src/database.java
+++ src/database.java
@@ -1,1 +1,24 @@
+package items;
+import java.io.File;
+public class Database {
+
+	public Database() {
+		// TODO Auto-generated constructor stub
+		//final Exception ReadIncompleteError = new Exception();
+		
+		public static void save(File file) throws Exception
+		{
+			
+		}
+		
+		public static void read(File file) throws Exception
+		{
+
+		}
+		
+		public static void updateFile(File file)throws  Exception
+		{
+			
+		}
+	}
 
