a479e05bdb4379d873c4410fd0a544db7aa89ad0
minafang71 <50028052+minafang71@users.noreply.github.com>
2019-05-09 13:09:49 -0400

    Add files via upload

---
+++ src/gui/Main.java
@@ -0,0 +1,23 @@
+// everyone
+// last updated 05-09-19
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.io.*;
+
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		GUIExtension1.initFrameAndParentPanel ();
+		
+		MainMenu.initGUI();
+		Help.initGUI();
+		Map.initGUI();
+		
+		GUIExtension1.showFirstPane(MainMenu.getPane());
+	
+	} // Main method
+} // Main class
