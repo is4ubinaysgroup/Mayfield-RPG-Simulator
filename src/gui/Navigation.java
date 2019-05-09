bf8c442271d2820e870b5dc23ed70c595c78ebde
minafang71 <50028052+minafang71@users.noreply.github.com>
2019-05-09 14:38:58 -0400

    Update Navigation.java

--- src/gui/Navigation.java
+++ src/gui/Navigation.java
@@ -3,6 +3,7 @@
 /* will be finalized when Database.save method is complete; possibly change to pop-up
 in a blank room/"overworld" where the user clicks on their character sprite */
 
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -32,7 +33,7 @@ public class Navigation
 		btn_Map.setForeground(Color.WHITE);
 		btn_Map.setBackground(new Color(0, 0, 204));
 		btn_Map.setFont(new Font("Copperplate Gothic Bold", Font.PLAIN, 40));
-		btn_Map.setBounds(250, 200, 300, 100);
+		btn_Map.setBounds(250, 100, 300, 100);
 		NavigationPane.add(btn_Map);
 		
 		btn_Map.addActionListener (new ActionListener ()   
@@ -50,7 +51,7 @@ public class Navigation
 		btn_Backpack.setForeground(Color.WHITE);
 		btn_Backpack.setBackground(new Color(0, 0, 204));
 		btn_Backpack.setFont(new Font("Copperplate Gothic Bold", Font.PLAIN, 40));
-		btn_Backpack.setBounds(250, 500, 300, 100);
+		btn_Backpack.setBounds(250, 400, 300, 100);
 		NavigationPane.add(btn_Backpack);
 		
 		btn_Backpack.addActionListener (new ActionListener ()   
@@ -68,7 +69,7 @@ public class Navigation
 		btn_Save.setBackground(new Color(0, 0, 204));
 		btn_Save.setForeground(Color.WHITE);
 		btn_Save.setFont(new Font("Copperplate Gothic Bold", Font.PLAIN, 40));
-		btn_Save.setBounds(250, 350, 300, 100);
+		btn_Save.setBounds(250, 250, 300, 100);
 		NavigationPane.add(btn_Save);
 		
 		btn_Save.addActionListener (new ActionListener ()   
@@ -85,8 +86,8 @@ public class Navigation
 		btn_Return = new JButton("Return");
 		btn_Return.setBackground(new Color(0, 0, 204));
 		btn_Return.setForeground(Color.WHITE);
-		btn_Return.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		btn_Return.setBounds(250, 650, 300, 100);
+		btn_Return.setFont(new Font("Copperplate Gothic Bold", Font.PLAIN, 40));
+		btn_Return.setBounds(250, 550, 300, 100);
 		NavigationPane.add(btn_Return);
 		
 		btn_Return.addActionListener (new ActionListener ()   
