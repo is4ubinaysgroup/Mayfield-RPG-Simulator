573f5fce774930db8d60ad99fa61a8e5773cb81a
571370 <571370@2452IR306153745.ad1.peelsb.com>
2019-05-13 09:19:42 -0400

    added

---
+++ src/character/NonPlayer.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package src.character;
+
+/**
+ * @author Carson Fujita
+ * May 13th 2019.
+ * Created human
+ */
+public class NonPlayer extends Human {
+
+	/**
+	 * 
+	 */
+	public NonPlayer() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
