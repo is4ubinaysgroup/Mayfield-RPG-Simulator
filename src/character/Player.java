389b5c3a218a3823a85b384a98976bdf26298ada
minafang71 <50028052+minafang71@users.noreply.github.com>
2019-05-13 14:07:55 -0400

    Update Player.java

--- src/character/Player.java
+++ src/character/Player.java
@@ -16,21 +16,41 @@ updated
 
 public class Player extends Human {
 		
-	private int coins;
-	private int level;
-
-
-	public Player() {} // default constuctor
+	public final static int PRODUCT_ARRAY_SPACE = 100;
+	public final static int WEAPON_ARRAY_SPACE = 9;
 	
-	public Player(int health, int defense, int maxHealth, Weapon equippedWeapon, String imagePath, int coins, int level)
+	private int level;
+	private int coins;
+	
+	private int numProducts;
+	private int productCapacity; // upgradable up to 100
+	private int numWeapons; // not completely necessary but saves a linear check for the first array space pointing to null
+	
+	
+	private Weapon weapons [];
+	private Product products [];
+
+
+	public Player() {} // default constructor
+	
+	public Player(int health, int defense, int maxHealth, Weapon equippedWeapon, String imagePath,
+					int coins, int level, int numProducts, int numWeapons, int productCapacity)
 	{
 		this.setHealth (health);
 		this.setDefense (defense);
-		this.setMaxHealth (maxHealth):
+		this.setMaxHealth (maxHealth);
 		this.setEquippedWeapon (equippedWeapon);
 		this.setImagePath (imagePath);
+		
 		this.coins = coins;
 		this.level = level;
+		
+		this.numProducts = numProducts;
+		this.productCapacity = productCapacity;
+		this.numWeapons = numWeapons;
+				
+		this.weapons [] = new Weapon [WEAPON_ARRAY_SPACE];
+		this.products [] = new Product [PRODUCT_ARRAY_SPACE];
 		
 	} // Player constuctor
 	
@@ -42,34 +62,35 @@ public class Player extends Human {
 					  
 	public void setLevel (int level) {this.level = level;}
 	public int getLevel () {return level;}
+	
+	public void setNumProducts (int numProducts) {this.numProducts = numProducts;}
+	public int getNumProducts () {return numProducts;}
+	
+	public void setNumWeapons (int numWeapons) {this.numWeapons = numWeapons;}
+	public int getNumWeapons () {return numWeapons;}
+	
+	public void setProductCapacity(int productCapacity) {this.productCapacity = productCapacity;}
+	public int getProductCapacity() {return productCapacity;}
+
+	public void setWeaponsArray (Weapon[] weapons) {this.weapons[] = weapons;}
+	public Weapon[] getWeaponsArray () {return weapons;}
+	
+	public void setProductsArray (Product[] products) {this.products[] = products;}
+	public Product[] getProductsArray () {return products;}
 
 	
 	// ------------------------------------------------ product and weapon storage management ------------------------------------------------
 
-	public boolean isCapacityUpgradable() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+	public boolean isCapacityUpgradable() {return numProducts < PRODUCT_ARRAY_SPACE;} // isCapacityUpgradable method
 	
-	public void upgradeCapacity() 
+	public void upgradeCapacity() {productCapacity = productCapacity+5;} // upgradeCapacity method
+
+
+	
+	public void deleteProduct(Product product)
 	{
 		
-	}
-
-	public void setProductCapacity(int productCapacity) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public int getProductCapacity() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	public void deleteProduct(Product product) {
-		// TODO Auto-generated method stub
-		
-	}
+	} // delete Product method
 
 
 
