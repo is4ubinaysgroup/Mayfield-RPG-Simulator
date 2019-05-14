fb185afc4366cf6b29b5d1c88a67e243fd95f796
Carson Fujita <50027005+TheFujirose@users.noreply.github.com>
2019-05-14 09:21:22 -0400

    Fixes
    asd

--- src/character/Human.java
+++ src/character/Human.java
@@ -15,33 +15,95 @@ Carson Fujita
 fixed imagepath
 */
 
+/*
+ date: May 14th 2019
+ Contributor:Carson Fujita
+ added location getter and setters along with x and y getter and setter.
+ added maxdefense; added maxdefense getter and setter.
+ added attack();
+ added hasLowHealth();
+*/
 import src.items.Weapon;
 
 public class Human
 {
 
 	public static int MOVEMENT = 3;
-
+	protected String name;
 	protected int health;
 	protected int defense;
+	protected int maxDefense;
 	protected int maxHealth;
 	protected Weapon equippedWeapon;
 	protected String imagePath;
+	private int[] position = new int[2];
+	
+	
+	public void attack(Human human) 
+	{
+		//Cuts damage from armor then if there is more damage it cuts to health. 
+		//also adds critical change 
+		int initialDamage =this.getEquippedWeapon().getDamage();
+		if (Math.random() <= this.equippedWeapon.getCriticalChance()) 
+		{
+			initialDamage= initialDamage*2;
+		}
+		int damageToHealth = human.getDefense() - initialDamage;
+		if(damageToHealth < 0) 
+		{
+			human.setDefense(0);
+			human.setHealth(human.getHealth() - damageToHealth);
+		}
+	}
+	
+	public boolean hasLowHealth() 
+	{
+		if(getHealth() <= 20 ) // I think 20 is little health
+		{
+			return true;
+		}
+		else 
+		{
+			return false;
+		}
+	}
 
 	// ------- gets and sets -------
 	public void setHealth (int health) {this.health = health;}
-	public int getHealth () {return health;}
+	public int getHealth () {return this.health;}
+	
+	public void setMaxDefense (int maxDefense) {this.maxDefense = maxDefense;}
+	public int getMaxDefense () {return this.maxDefense;}
 	
 	public void setDefense (int defense) {this.defense = defense;}
-	public int getDefense () {return defense;}
+	public int getDefense () {return this.defense;}
 	
 	public void setMaxHealth (int maxHealth) {this.maxHealth = maxHealth;}
-	public int getMaxHealth () {return maxHealth;}
+	public int getMaxHealth () {return this.maxHealth;}
 	
 	public void setEquippedWeapon (Weapon equippedWeapon) {this.equippedWeapon = equippedWeapon;}
-	public Weapon getEquippedWeapon () {return equippedWeapon;}
+	public Weapon getEquippedWeapon () {return this.equippedWeapon;}
 	
 	public void setImagePath (String imagePath) {this.imagePath = imagePath;}
-	public String getImagePath () {return imagePath;}
+	public String getImagePath () {return this.imagePath;}
+	public int[] getPosition() {
+		return position;
+	}
+	public void setPosition(int[] position) {
+		this.position = position;
+	}
+	public int getY() {
+		return this.position[1];
+	}
+	public void setY(int y) {
+		this.position[1] = y;
+	}
+	public int getX() {
+		return this.position[0];
+	}
+	public void setX(int x) {
+		this.position[0] = x;
+	}
+
 
 } // Human class
