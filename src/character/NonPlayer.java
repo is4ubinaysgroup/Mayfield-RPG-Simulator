c1af210a5a34208f3d758fdf7e635f3f0e139e97
Carson Fujita <50027005+TheFujirose@users.noreply.github.com>
2019-05-16 08:58:09 -0400

    Carson Fujita edit
    coded more to runTurn()
    added nearCorner()

--- src/character/NonPlayer.java
+++ src/character/NonPlayer.java
@@ -15,6 +15,14 @@ import src.gui.MainExecutable;
  * created isBoss with setters and getters
  * created runTurn()
  */
+/*
+may 16th 2019
+Carson Fujita
+coded more to runTurn()
+added nearCorner()
+
+
+*/
 public class NonPlayer extends Human {
 
 	/**
@@ -51,6 +59,7 @@ UML
 + setToHallMonitor(Boolean)
 
 	 */
+	
 	private Boolean isBoss;
 	
 	public NonPlayer(String name,Boolean isBoss) 
@@ -69,27 +78,68 @@ UML
 	
 	public void runTurn(Room room) //TODO
 	{
-		int r = MainExecutable.getPlayer().getEquippedWeapon().getRange();
-		int x =MainExecutable.getPlayer().getX();
-		int y = MainExecutable.getPlayer().getY();
-		
-		Point node1 = new Point(x+r, y+r);
-		Point node2 = new Point(x+r, y-r);
-		Point node3 = new Point(x-r, y-r);
-		Point node4 = new Point(x-r,y+r);
-		
-	
-		
-		if(MainExecutable.getPlayer().hasLowHealth()) //if Player has low health
+		Player player = MainExecutable.getPlayer();
+		int r = player.getEquippedWeapon().getRange();
+
+
+
+		if(player.hasLowHealth()) //if Player has low health
 		{
-			
+
 		}
-		else if( ( r > Human.MOVEMENT ) && (this.getX() >= node4.x && this.getX() <= node2.x &&
-				this.getY() >= node4.y && this.getY() <= node2.y) && () ) // if cornered
+		else if( ( r < Human.MOVEMENT)  && inRangeOf(player) && nearCorner() ) // if cornered
 		{
-			
+
 		}
-			
+		else if(player.inRangeOf(this) && ( player.getEquippedWeapon().getType() == Weapon.MELEETYPE || player.getEquippedWeapon().getType() == Weapon.ALLTYPE ))
+		{// 3. If the player is in melee attack range:
+
+			//The boss will check and follow the first true statement procedure in order of first to last:
+			if(MainExecutable.getPlayer().hasLowHealth()) //1. If the player has low health:
+			{
+			}
+			else if( ( r < Human.MOVEMENT)  &&  inRangeOf(player)  && nearCorner())//2. If the boss is cornered:
+			{
+				//The boss will move to the other side of the player, however, if the boss cannot it will move as far as it can to the other side of the player.
+
+			}
+		}
+		//Continues to the next true statement
+		if(player.inRangeOf(this) && ( player.getEquippedWeapon().getType() == Weapon.RANGEDTYPE || player.getEquippedWeapon().getType() == Weapon.ALLTYPE ))//If the player is in non-melee attack range:
+		{
+			/*
+				 Based on a random number from 1-2 the boss will:
+				If 1, attack the player.
+				If 2, move out of melee range but into non-melee range, then attack.
+				End of turn.
+			 */
+		}
+		else	if(!(player.inRangeOf(this))) 
+		{
+			/*
+			 * 5. player is not in range:
+			 * 	Based on a random number from 1-2 the boss will:
+				If 1, The boss will move only as close as it can to ensure the player is in it's melee range, however, if the boss can do this and be out of the players attack range it will move to the location that it can attack and be out of player range.
+				If 2, The boss will move only as close as it can to ensure the player is in it's non-melee range, however, if the boss can do this and be out of the players attack range it will move to the location that it can attack and be out of player range.
+				End of turn.
+			 */
+		}
+		else		if(inRangeOf(player)) 
+		{
+			/*
+				 6. The Boss is in player range
+				The boss will move out of player range and recover 1% of its health.
+				End of turn.
+			 */
+		}
+		else {}
+
+	}		
+
+
+	private boolean nearCorner() {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 	
