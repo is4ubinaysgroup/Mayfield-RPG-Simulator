a2833c6776ee8990717841f31fc65e7651fab8b6
minafang71 <50028052+minafang71@users.noreply.github.com>
2019-05-25 10:47:53 -0400

    Update Navigation.java

--- src/gui/Navigation.java
+++ src/gui/Navigation.java
@@ -36,6 +36,15 @@ public class Navigation
 
 	public static void initGUI()
 	{
+		
+		// ------- navigationPane -------
+		NavigationPane = new JPanel();
+		NavigationPane.setBackground(Database.ALT_MAYFIELD_BLUE);
+		NavigationPane.setLayout(null);
+				
+				
+				
+		// ------- local background -------
 		JPanel background = new JPanel() 
 		{
 			public void paint(Graphics g) 
@@ -45,10 +54,6 @@ public class Navigation
 		};
 		background.setBounds(0, 0, 1000, 800);
 		background.setVisible(true);
-		// ------- navigationPane -------
-		NavigationPane = new JPanel();
-		NavigationPane.setBackground(Database.ALT_MAYFIELD_BLUE);
-		NavigationPane.setLayout(null);
 		
 		
 		
@@ -60,12 +65,22 @@ public class Navigation
 		btn_Map.setBounds(350, 100, 300, 100);
 		NavigationPane.add(btn_Map);
 		
-		btn_Map.addActionListener (new ActionListener ()   
+		btn_Map.addMouseListener (new MouseListener()   
 		{
-			public void actionPerformed (ActionEvent e)
+			public void mouseClicked(MouseEvent arg0)
 			{
 				GUIExtension1.switchPane(Map.getPane());
+				playSound(Database.getPageTurnSound());
 			}
+			
+			public void mouseEntered(MouseEvent arg0)
+			{
+				playSound(Database.getHoverSound1());
+			}
+
+			public void mouseExited(MouseEvent arg0) {}
+			public void mousePressed(MouseEvent arg0) {}
+			public void mouseReleased(MouseEvent arg0) {}
 		});
 		
 		
@@ -78,17 +93,24 @@ public class Navigation
 		btn_Backpack.setBounds(350, 400, 300, 100);
 		NavigationPane.add(btn_Backpack);
 		
-		btn_Backpack.addActionListener (new ActionListener ()   
+		btn_Backpack.addMouseListener (new MouseListener()   
 		{
-			public void actionPerformed (ActionEvent e)
+			public void mouseClicked(MouseEvent arg0)
 			{
-				
 				GUIExtension1.switchPane(Backpack.getPane());
 				Backpack.update();
 				
 				playSound(Database.getBackpackBtnSound());
-				
 			}
+			
+			public void mouseEntered(MouseEvent arg0)
+			{
+				playSound(Database.getHoverSound1());
+			}
+
+			public void mouseExited(MouseEvent arg0) {}
+			public void mousePressed(MouseEvent arg0) {}
+			public void mouseReleased(MouseEvent arg0) {}
 		});
 		
 		
@@ -101,15 +123,22 @@ public class Navigation
 		btn_Save.setBounds(350, 250, 300, 100);
 		NavigationPane.add(btn_Save);
 		
-		btn_Save.addActionListener (new ActionListener ()   
+		btn_Save.addMouseListener (new MouseListener()   
 		{
-			public void actionPerformed (ActionEvent e)
+			public void mouseClicked(MouseEvent arg0)
 			{
 				Database.writeSingle();
-				
-				playSound(Database.getSaveBtnSound());
-
+				playSound(Database.getMooSound());
 			}
+			
+			public void mouseEntered(MouseEvent arg0)
+			{
+				playSound(Database.getHoverSound1());
+			}
+
+			public void mouseExited(MouseEvent arg0) {}
+			public void mousePressed(MouseEvent arg0) {}
+			public void mouseReleased(MouseEvent arg0) {}
 		});
 		
 		
@@ -122,16 +151,29 @@ public class Navigation
 		btn_Return.setBounds(350, 550, 300, 100);
 		NavigationPane.add(btn_Return);
 		
-		btn_Return.addActionListener (new ActionListener ()   
+		btn_Return.addMouseListener (new MouseListener()   
 		{
-			public void actionPerformed (ActionEvent e)
+			public void mouseClicked(MouseEvent arg0)
 			{
 				GUIExtension1.switchPane(MainMenu.getPane());
+				playSound(Database.getSelectTone1());
 			}
+
+			public void mouseEntered(MouseEvent arg0)
+			{
+				playSound(Database.getHoverSound1());
+			}
+
+			public void mouseExited(MouseEvent arg0) {}
+			public void mousePressed(MouseEvent arg0) {}
+			public void mouseReleased(MouseEvent arg0) {}
 		});
-		NavigationPane.add(background);
 
 		
+		
+		// add the background
+		NavigationPane.add(background);
+
 	} // initGUI method
 	
 	
