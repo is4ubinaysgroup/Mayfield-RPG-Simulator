5aa40c674b6f5e623d7b2b46137eebc166239994
ZacGomez <50029089+ZacGomez@users.noreply.github.com>
2019-05-27 08:54:32 -0400

    Add files via upload

--- src/gui/CombatMenu.java
+++ src/gui/CombatMenu.java
@@ -1,4 +1,6 @@
-package src.gui;
+/* Zac 5/27/19
+ * added button color and font
+ */
 
 import javax.swing.JPanel;
 import java.awt.Color;
@@ -28,22 +30,22 @@ public class CombatMenu extends JPanel {
 		CombatPanel.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("COMBAT MENU");
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblNewLabel.setFont(new Font("Copperplate Gothic Bold", Font.PLAIN, 20));
 		lblNewLabel.setBounds(33, 13, 135, 29);
 		CombatPanel.add(lblNewLabel);
 		
 		JLabel lblHealth = new JLabel("HEALTH");
-		lblHealth.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblHealth.setFont(new Font("Copperplate Gothic Bold", Font.PLAIN, 15));
 		lblHealth.setBounds(12, 80, 56, 16);
 		CombatPanel.add(lblHealth);
 		
 		JLabel lblDefence = new JLabel("DEFENCE");
-		lblDefence.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblDefence.setFont(new Font("Copperplate Gothic Bold", Font.PLAIN, 15));
 		lblDefence.setBounds(12, 118, 66, 16);
 		CombatPanel.add(lblDefence);
 		
 		JLabel lblMovement = new JLabel("MOVEMENT");
-		lblMovement.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblMovement.setFont(new Font("Copperplate Gothic Bold", Font.PLAIN, 15));
 		lblMovement.setBounds(12, 163, 80, 16);
 		CombatPanel.add(lblMovement);
 		
@@ -53,6 +55,9 @@ public class CombatMenu extends JPanel {
 			}
 		});
 		btnAttack.setBounds(33, 271, 122, 46);
+		btnAttack.setFont(new Font("Copperplate Gothic Bold", Font.PLAIN, 17));
+		btnAttack.setForeground(new Color(247, 221, 0));
+		btnAttack.setBackground(new Color(14, 1, 141));
 		CombatPanel.add(btnAttack);
 		
 		JButton btnMove = new JButton("MOVE");
@@ -61,6 +66,9 @@ public class CombatMenu extends JPanel {
 				//moveTo(Point point);
 			}
 		});
+		btnMove.setFont(new Font("Copperplate Gothic Bold", Font.PLAIN, 17));
+		btnMove.setForeground(new Color(247, 221, 0));
+		btnMove.setBackground(new Color(14, 1, 141));
 		btnMove.setBounds(33, 369, 122, 46);
 		CombatPanel.add(btnMove);
 		
@@ -69,6 +77,9 @@ public class CombatMenu extends JPanel {
 			public void actionPerformed(ActionEvent e) {//-------------------------- Opens Backpack Panel ---------------------
 			}
 		});
+		btnBackpack.setFont(new Font("Copperplate Gothic Bold", Font.PLAIN, 17));
+		btnBackpack.setForeground(new Color(247, 221, 0));
+		btnBackpack.setBackground(new Color(14, 1, 141));
 		btnBackpack.setBounds(33, 464, 122, 46);
 		CombatPanel.add(btnBackpack);
 		
@@ -77,21 +88,24 @@ public class CombatMenu extends JPanel {
 			public void actionPerformed(ActionEvent arg0) {// ---------------------- Opens Pause Panel -------------------------
 			}
 		});
+		btnPause.setFont(new Font("Copperplate Gothic Bold", Font.PLAIN, 17));
+		btnPause.setForeground(new Color(247, 221, 0));
+		btnPause.setBackground(new Color(14, 1, 141));
 		btnPause.setBounds(33, 552, 122, 46);
 		CombatPanel.add(btnPause);
 		
 		JLabel lblHealthInt = new JLabel("");
-		lblHealthInt.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblHealthInt.setFont(new Font("Copperplate Gothic Bold", Font.PLAIN, 15));
 		lblHealthInt.setBounds(99, 80, 56, 16);
 		CombatPanel.add(lblHealthInt);
 		
 		JLabel lblDefenceInt = new JLabel("");
-		lblDefenceInt.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblDefenceInt.setFont(new Font("Copperplate Gothic Bold", Font.PLAIN, 15));
 		lblDefenceInt.setBounds(90, 118, 56, 16);
 		CombatPanel.add(lblDefenceInt);
 		
 		JLabel lblMovementInt = new JLabel("");
-		lblMovementInt.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblMovementInt.setFont(new Font("Copperplate Gothic Bold", Font.PLAIN, 15));
 		lblMovementInt.setBounds(112, 163, 56, 16);
 		CombatPanel.add(lblMovementInt);
 
