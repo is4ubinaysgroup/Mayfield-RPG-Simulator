6c75b4318f2a436f1f4d01fda0aef2a259f82086
minafang71 <50028052+minafang71@users.noreply.github.com>
2019-05-27 19:06:26 -0400

    CombatMenu update

--- src/gui/CombatMenu.java
+++ src/gui/CombatMenu.java
@@ -1,3 +1,5 @@
+package src.gui;
+
 /* Zac 5/27/19
  * added button color and font
  */
@@ -23,97 +25,118 @@ public class CombatMenu extends JPanel {
 		setBackground(Color.GRAY);
 		setLayout(null);
 		
+		// ------- CombatPanel -------
 		JPanel CombatPanel = new JPanel();
 		CombatPanel.setBounds(800, 0, 200, 800);
 		CombatPanel.setBackground(Color.LIGHT_GRAY);
-		add(CombatPanel);
 		CombatPanel.setLayout(null);
+		
+		add(CombatPanel);
+		
+		
+		// labels
 		
 		JLabel lblNewLabel = new JLabel("COMBAT MENU");
 		lblNewLabel.setFont(new Font("Copperplate Gothic Bold", Font.PLAIN, 20));
 		lblNewLabel.setBounds(33, 13, 135, 29);
 		CombatPanel.add(lblNewLabel);
 		
+		
+		// health
 		JLabel lblHealth = new JLabel("HEALTH");
 		lblHealth.setFont(new Font("Copperplate Gothic Bold", Font.PLAIN, 15));
 		lblHealth.setBounds(12, 80, 56, 16);
 		CombatPanel.add(lblHealth);
-		
-		JLabel lblDefence = new JLabel("DEFENCE");
-		lblDefence.setFont(new Font("Copperplate Gothic Bold", Font.PLAIN, 15));
-		lblDefence.setBounds(12, 118, 66, 16);
-		CombatPanel.add(lblDefence);
-		
-		JLabel lblMovement = new JLabel("MOVEMENT");
-		lblMovement.setFont(new Font("Copperplate Gothic Bold", Font.PLAIN, 15));
-		lblMovement.setBounds(12, 163, 80, 16);
-		CombatPanel.add(lblMovement);
-		
-		JButton btnAttack = new JButton("ATTACK");
-		btnAttack.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {//-------------------------- Attack Methods --------------------------
-			}
-		});
-		btnAttack.setBounds(33, 271, 122, 46);
-		btnAttack.setFont(new Font("Copperplate Gothic Bold", Font.PLAIN, 17));
-		btnAttack.setForeground(new Color(247, 221, 0));
-		btnAttack.setBackground(new Color(14, 1, 141));
-		CombatPanel.add(btnAttack);
-		
-		JButton btnMove = new JButton("MOVE");
-		btnMove.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {//-------------------------- Move Methods -----------------------------
-				//moveTo(Point point);
-			}
-		});
-		btnMove.setFont(new Font("Copperplate Gothic Bold", Font.PLAIN, 17));
-		btnMove.setForeground(new Color(247, 221, 0));
-		btnMove.setBackground(new Color(14, 1, 141));
-		btnMove.setBounds(33, 369, 122, 46);
-		CombatPanel.add(btnMove);
-		
-		JButton btnBackpack = new JButton("BACKPACK");
-		btnBackpack.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {//-------------------------- Opens Backpack Panel ---------------------
-			}
-		});
-		btnBackpack.setFont(new Font("Copperplate Gothic Bold", Font.PLAIN, 17));
-		btnBackpack.setForeground(new Color(247, 221, 0));
-		btnBackpack.setBackground(new Color(14, 1, 141));
-		btnBackpack.setBounds(33, 464, 122, 46);
-		CombatPanel.add(btnBackpack);
-		
-		JButton btnPause = new JButton("PAUSE");
-		btnPause.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {// ---------------------- Opens Pause Panel -------------------------
-			}
-		});
-		btnPause.setFont(new Font("Copperplate Gothic Bold", Font.PLAIN, 17));
-		btnPause.setForeground(new Color(247, 221, 0));
-		btnPause.setBackground(new Color(14, 1, 141));
-		btnPause.setBounds(33, 552, 122, 46);
-		CombatPanel.add(btnPause);
 		
 		JLabel lblHealthInt = new JLabel("");
 		lblHealthInt.setFont(new Font("Copperplate Gothic Bold", Font.PLAIN, 15));
 		lblHealthInt.setBounds(99, 80, 56, 16);
 		CombatPanel.add(lblHealthInt);
 		
-		JLabel lblDefenceInt = new JLabel("");
-		lblDefenceInt.setFont(new Font("Copperplate Gothic Bold", Font.PLAIN, 15));
-		lblDefenceInt.setBounds(90, 118, 56, 16);
-		CombatPanel.add(lblDefenceInt);
+		
+		// defense
+		JLabel lblDefense = new JLabel("DEFENSE");
+		lblDefense.setFont(new Font("Copperplate Gothic Bold", Font.PLAIN, 15));
+		lblDefense.setBounds(12, 118, 66, 16);
+		CombatPanel.add(lblDefense);
+		
+		JLabel lblDefenseInt = new JLabel("");
+		lblDefenseInt.setFont(new Font("Copperplate Gothic Bold", Font.PLAIN, 15));
+		lblDefenseInt.setBounds(90, 118, 56, 16);
+		CombatPanel.add(lblDefenseInt);
+		
+		
+		// movement
+		JLabel lblMovement = new JLabel("MOVEMENT");
+		lblMovement.setFont(new Font("Copperplate Gothic Bold", Font.PLAIN, 15));
+		lblMovement.setBounds(12, 163, 80, 16);
+		CombatPanel.add(lblMovement);
 		
 		JLabel lblMovementInt = new JLabel("");
 		lblMovementInt.setFont(new Font("Copperplate Gothic Bold", Font.PLAIN, 15));
 		lblMovementInt.setBounds(112, 163, 56, 16);
 		CombatPanel.add(lblMovementInt);
+		
+		
+		
+		// ------- btnAttack -------
+		JButton btnAttack = new JButton("ATTACK");
+		btnAttack.setBounds(33, 271, 122, 46);
+		btnAttack.setFont(new Font("Copperplate Gothic Bold", Font.PLAIN, 17));
+		btnAttack.setForeground(new Color(247, 221, 0));
+		btnAttack.setBackground(new Color(14, 1, 141));
+		CombatPanel.add(btnAttack);
+		
+		btnAttack.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {//-------------------------- Attack Methods --------------------------
+			}
+		});
+		
+		
+		
+		// ------- btnBackpack -------
+		JButton btnBackpack = new JButton("BACKPACK");
+		btnBackpack.setFont(new Font("Copperplate Gothic Bold", Font.PLAIN, 17));
+		btnBackpack.setForeground(new Color(247, 221, 0));
+		btnBackpack.setBackground(new Color(14, 1, 141));
+		btnBackpack.setBounds(33, 464, 122, 46);
+		CombatPanel.add(btnBackpack);
+		
+		btnBackpack.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {//-------------------------- go to Backpack Panel ---------------------
+			
+				//GUIExtension1.switchPane(Backpack.getPane());
+			
+			}
+		});
+		
+		
 
-	}
+		JButton btnMove = new JButton("MOVE");
+		btnMove.setFont(new Font("Copperplate Gothic Bold", Font.PLAIN, 17));
+		btnMove.setForeground(new Color(247, 221, 0));
+		btnMove.setBackground(new Color(14, 1, 141));
+		btnMove.setBounds(33, 369, 122, 46);
+		CombatPanel.add(btnMove);
+		
+		btnMove.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {//-------------------------- Move Methods -----------------------------
+				//moveTo(Point point);
+			}
+		});
+		
+		
+	} // CombatMenu constructor
+	
+	
+	
 	public static void AttackMethod() {
 		//Select Enemy and check if in range.
 		
 		
 		
-	}
-}
+	} // AttackMethod method
+	
+	
+	
+} // CombatMenu class
